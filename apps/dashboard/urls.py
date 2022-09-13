9396025e14f83cf24ed47ce32f7b283ba6c710f2
taxilsavani <taxil.savani@gmail.com>
2022-09-13 13:05:07 -0400

    Logo and download button changes

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -6,6 +6,7 @@ from apps.dashboard import views
 urlpatterns = [
 
     # The home page
+    path('dashboard/', views.index, name='dashboard'),
     path('', views.index, name='dashboard'),
     path("create_csv/", views.create_csv, name='csv'),
  
