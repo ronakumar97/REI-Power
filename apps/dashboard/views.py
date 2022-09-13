51cd733be5f616980caffe8a337abdf12a938762
taxilsavani <taxil.savani@gmail.com>
2022-09-13 00:26:11 -0400

    UI changes

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -12,6 +12,7 @@ from django.views.decorators.csrf import csrf_exempt
 import csv
 import numpy as np
 
+
 def index(request):
     context = {}
     context['segment'] = 'dashboard.html'
@@ -48,14 +49,14 @@ def condensor_water_reset_temp(CDWRT, CP2CHOAT):
     except:
         return np.NaN
 
-def fault_rule_implementation():
-    df = pd.read_csv('/home/ubuntu/PycharmProjects/rei_power/column_mapping.csv', header=None)
+def fault_rule_implementation(data_file,mapping_file):
+    df = pd.read_csv(mapping_file, header=None)
     column_mappings = {}
     for index, row in df.iterrows():
         row[0] = row[0].split(':')[0]
         column_mappings[row[0]] = row[1]
 
-    df = pd.read_csv('/home/ubuntu/PycharmProjects/rei_power/CP2 COOLING_data.csv', sep="\t")
+    df = pd.read_csv(data_file, sep="\t")
     df = df.iloc[:, :-1]
     df = df.rename(columns={'<>Date': 'Date'})
     df = df.rename(columns=column_mappings)
@@ -72,11 +73,10 @@ def create_csv(request):
     if request.method == 'POST':
 
         # TODO: Make two buttons (one for uploading the column mapping and other for uploading the data in the CSV)
-        csv_file = request.FILES["csv_file"]
-        df = pd.read_csv(csv_file)
-        df = df.reset_index(drop=True)
-
-        df = fault_rule_implementation()
+        data_file = request.FILES["csv_file"]
+        mapping_file = request.FILES["Mapping_file"]
+              
+        df = fault_rule_implementation(data_file,mapping_file)
 
         df['is_free_cooling_operation_results'] = df.apply(
             lambda row: is_free_cooling_operation(row['CP2.CHOAT'], row['CP2.CH1.M5'], row['CP2.CH2.M10']), axis=1)
@@ -90,20 +90,28 @@ def create_csv(request):
             lambda row: condensor_water_reset_temp(row['CDWRT'], row['CP2.CHOAT']), axis=1)
 
         # TODO: Dropdown to select which faults to choose
+         # result = df.to_html(index=False)
+        df['DateTime'] = df[['Date', 'Time']].apply(lambda x: ' '.join(x), axis=1)
+        
+        filter_col=['DateTime','is_free_cooling_operation_results','chiller_water_temp_diff_results','condensor_water_temp_diff_results','condensor_water_reset_temp_results']
+        df = df[filter_col].T
+        new_header = df.iloc[0] 
+        df = df[1:]
+        df.columns = new_header 
 
-        result = df.to_html(index=False)
-
-        # print(df)
+        filter_col_name=['Free cooling operation results','Chiller water temp diff results','Condensor water temp diff results','Condensor water reset temp results']
+        df.insert(loc=0, column='Fault Rules', value=filter_col_name)
+       
         request.session['result'] = df.to_json(orient="records")
         html_template = loader.get_template('home/dashboard.html')
-        return HttpResponse(html_template.render({"html":result, "dataframe":df}, request))
+        return HttpResponse(html_template.render({"dataframe":df}, request))
                    
     else:
         df = pd.DataFrame()
         if request.session.get('result'):
             result =  request.session.get('result')
             df = pd.DataFrame(json.loads(result))
-            print(df)
+            # print(df)
         response = HttpResponse(df.to_csv(index=False),content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
         response['Content-Disposition'] = 'attachment; filename="sensor_data_results.csv"'
         return response
